47dbb1ab4fc0178c8bfecedd777e58138cb53d79
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-10-02 12:41:18 +0530

    add some more logic to append in dict

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ def readWords(fName, pageNumber):
     with open(fName, 'r') as file:
         for line in file:
             for word in line.split():
-                indexDict[word] = pageNumber
+                if word not in indexDict.keys():
+                    indexDict[word] = pageNumber
+                else:
+                    if indexDict[word] != pageNumber:
+                        indexDict[word] = [indexDict[word], pageNumber]
 
 
 if __name__ == "__main__":
     readWords("demo.txt", 1)
+    readWords("demo2.txt", 2)
     for key in indexDict.keys():
         lineValue = f"{key} : {indexDict[key]}"
         print(lineValue)
